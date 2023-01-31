f38468d0c733685488d30da752fb34f453e29dff
Lowell Thoerner <lowellashton@gmail.com>
2023-01-30 23:28:52 -0800

    Added window size fields for Editor.

--- src/editor.rs
+++ src/editor.rs
@@ -1,12 +1,16 @@
 use std::fs::{File, OpenOptions};
 
 use ropey::Rope;
+use crossterm::terminal;
 
 // Represents the state of the editor
 // There should only be one instance of this struct at any given point
+#[allow(dead_code)]
 pub struct Editor {
     file: File,
     buffer: Rope,
+    window_length: usize,
+    window_height: usize,
 }
 
 impl Editor {
@@ -23,8 +27,15 @@ impl Editor {
         // Read the file into a Rope
         let buffer = Rope::from_reader(&file).unwrap();
 
-        // Return the editor
-        Self { file, buffer }
+        // Get the terminal size
+        let window_size = terminal::size().expect("[INTERNAL ERROR] Failed to get terminal size");
+
+        Self {
+            file,
+            buffer,
+            window_length: window_size.0 as usize,
+            window_height: window_size.1 as usize,
+        }
     }
 
     // Opens the editor in the terminal and runs the event loop
